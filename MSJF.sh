7de29ee19568ac2751e27ae70576c84492c6060e
B4nri <97583276+diositobanri@users.noreply.github.com>
2022-11-27 01:20:05 -0300

    Add files via upload

---
+++ MSJF.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Colours
+redColour="\e[0;31m\033[1m"
+redColour="\e[0;31m\033[1m"
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+clear
+
+echo -e ${blueColour}"Cómo estas? ¿Cuanto tiempo no?"
+echo -e "espero que sea de tu agrado este comunicado, aunque te doy por seguro que no lo
+sera..."
+echo -e "Me he percatado de que tu no has tomado el consejo que yo te he consedido, 
+por consecuencia, ahora viene la segunda parte de este encuentro..."
+echo -e "Esta es una advertencia, lo que le paso a ${redColour}tu querida madre ${blueColour}no ha sido 
+casualidad, han habido manos de por medio"
+echo -e "O como por ejemplo a le puede pasar algo malo a ${redColour}juanita..."
+echo -e ${redColour}"A vos no te va a pasar nada, pero a ${redColour}tus seres queridos ${blueColour}tal vez..."
+echo -e "De la unica forma que no ocurra ningun accidente contundente..."
+echo -e "Es que vos, katfla, te olvides de toda relacion en la que no te agraden."
+echo -e "En el momento, te van a decir, ${redColour}'vos no me agradas', ${blueColour}y ahi vas a reaccionar
+de forma instantanea"
+echo -e ${greenColour}"Yo soy el todo, de una divina providencia, yo escucho todas tus calumnias
+y estupideses, yo te veo en todo momento, yo soy
+tu sombra, y viviras con ello..."
+
+sleep 80
+clear
